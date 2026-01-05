f8dc1085e303f71d14b346762f94e7d91170dad3
larrison23 <102928637+larrison23@users.noreply.github.com>
2026-01-05 14:45:21 -0500

    Shopping List interface

---
+++ app/src/main/java/recipeapp/ShoppingList.java
@@ -0,0 +1,20 @@
+package recipeapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingList {
+    private final Map<Ingredient, RecipeIngredient> items = new HashMap<>();
+
+    public void addRecipe(Recipe recipe) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addIngredient(Ingredient ingredient, double qty, String unit) {
+        throw new UnsupportedOperationException();
+    }
+
+    public string toString() {
+        throw new UnsupportedOperationException();
+    }
+}

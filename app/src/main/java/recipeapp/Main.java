5a4f4a749827081e83e9bfe45a5c4b9f725c7af1
larrison23 <102928637+larrison23@users.noreply.github.com>
2026-01-06 12:04:26 -0500

    New Recipes to main

--- app/src/main/java/recipeapp/Main.java
+++ app/src/main/java/recipeapp/Main.java
@@ -19,4 +19,28 @@ public class Main {
 
         return chicPic;
     }
+
+    private static Recipe bakedPasta() {
+        Recipe bakedPasta = new Recipe("Baked Pasta");
+
+        bakedPasta.addIngredient(new Ingredient("Crushed Tomatoes", Location.PANTRY), 2, "28-ounce can", null);
+        bakedPasta.addIngredient(new Ingredient("Butter", Location.DAIRY), 1, "stick", null);
+        bakedPasta.addIngredient(new Ingredient("Mozzarella", Location.DAIRY), 6, "ounces", "sliced");
+        bakedPasta.addIngredient(new Ingredient("Parmesan", Location.DAIRY), 0.5, "cups", "grated");
+        bakedPasta.addIngredient(new Ingredient("Ricotta", Location.DAIRY), 0.5, "cups", null);
+        bakedPasta.addIngredient(new Ingredient("Cavatappi", Location.PANTRY), 1, "lb", null);
+
+        return bakedPasta;
+    }
+
+    private static Recipe lemonPepChic() {
+        Recipe lemonPepChic = new Recipe("Lemon Pepper Chicken");
+
+        lemonPepChic.addIngredient(new Ingredient("Lemon", Location.PRODUCE), 4, "items", "zest");
+        lemonPepChic.addIngredient(new Ingredient("Bone-In, Skin-On Chicken Thighs", Location.MEAT), 2, "lbs", null);
+        lemonPepChic.addIngredient(new Ingredient("Chicken Stock", Location.PANTRY), 0.5, "cups", null);
+        lemonPepChic.addIngredient(new Ingredient("Large Yukon Gold Potatoes", Location.PRODUCE), 3, "lbs", "peeled and cut into 8");
+
+        return lemonPepChic;
+    }
 }
